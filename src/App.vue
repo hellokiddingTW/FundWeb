12b6f124318f0a333c11b0808481a07007a3aeac
Nate Yeh <a498390344@gmail.com>
2021-06-29 15:10:43 +0800

    cdn

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,83 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <nav class="navbar navbar-expand-lg navbar-light container py-3">
+        <div class="container-fluid">
+          <a class="navbar-brand me-4" href="#">
+            <img
+              src="https://hexschool.github.io/boootstrap5WebLayout/assets/logo.jpg"
+              alt=""
+              width="111"
+              height="40"
+            />
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item px-3">
+                <a class="nav-link nav-linkColor" aria-current="page" href="#">關於</a>
+              </li>
+              <li class="nav-item px-3">
+                <a class="nav-link nav-linkColor" href="#">挖寶</a>
+              </li>
+              <li class="nav-item px-3">
+                <a class="nav-link nav-linkColor" href="#" tabindex="-1" aria-disabled="true"
+                  >我有個大膽的想法</a
+                >
+              </li>
+            </ul>
+            <form class="d-flex">
+              <button class="btn  btnRounded  btnPd me-3" type="submit">
+                登入
+              </button>
+              <button class="btn btn-outline-secondary btnRounded btnPd " type="submit">
+                註冊
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
 @import "~bootstrap/scss/bootstrap";
-// #app {
-//   font-family: Avenir, Helvetica, Arial, sans-serif;
-//   -webkit-font-smoothing: antialiased;
-//   -moz-osx-font-smoothing: grayscale;
-//   text-align: center;
-//   color: #2c3e50;
-// }
 
-// #nav {
-//   padding: 30px;
+#nav{
+  border-bottom: 5px solid #FFDF65;
+}
 
-//   a {
-//     font-weight: bold;
-//     color: #2c3e50;
+form{
+  button{
+    font-family: NotoSansCJKtc-Bold;
+  }
+  button:nth-child(1) {
+   color: #636057;
+   background-color: #FFDF65;
+   border: none;
+  }
+.btnPd{
+    padding:.5rem 2.5rem;
+  }
+.btnRounded{
+  border-radius: 19px;
+  border: 2px solid #858377;
+  }
+}
 
-//     &.router-link-exact-active {
-//       color: #42b983;
-//     }
-//   }
-// }
+
+
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,43 +1,149 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-<button type="button" class="btn btn-primary">Primary</button>
-<button type="button" class="btn btn-secondary">Secondary</button>
-<button type="button" class="btn btn-success">Success</button>
-<button type="button" class="btn btn-danger">Danger</button>
-<button type="button" class="btn btn-warning">Warning</button>
-<button type="button" class="btn btn-info">Info</button>
-<button type="button" class="btn btn-light">Light</button>
-<button type="button" class="btn btn-dark">Dark</button>
+  <div>
+    <main>
+      <div class="banner bg-light">
+        <div class="container py-5">
+          <div class="tittle mb-4">
+            <span class="badge bg-secondary">魔法科技</span>
+            <h2 class="lh-base">拍出會動的照片｜LivePhotos 魔法拍立得</h2>
+          </div>
+          <div class="content">
+            <div class="row">
+              <div class="col-md-6">
+                <img
+                  class="w-100"
+                  src="https://hexschool.github.io/boootstrap5WebLayout/assets/produt_image.jpg"
+                  alt=""
+                />
+              </div>
+              <div class="col-md-6">
+                <div class="mb-3">
+                  <span class="subText">目標$600,000</span>
+                  <h3>$280,047</h3>
+                  <div class="progress">
+                    <div
+                      class="progress-bar bg-theme text-secondary"
+                      role="progressbar"
+                      style="width: 41%"
+                      aria-valuenow="41"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                    >
+                      41%
+                    </div>
+                  </div>
+                </div>
+                <div class="row mb-2">
+                  <div class="col-6">
+                    <span class="subText">贊助人數</span>
+                    <h3>1374 人</h3>
+                  </div>
+                  <div class="col-6">
+                    <span class="subText">募資倒數</span>
+                    <h3>27 天</h3>
+                  </div>
+                </div>
+                <div class="icon mb-3">
+                  <img
+                    class="me-4"
+                    src="https://hexschool.github.io/boootstrap5WebLayout/assets/icon_good01.svg"
+                    alt=""
+                  />
+                  <img
+                    class="me-4"
+                    src="https://hexschool.github.io/boootstrap5WebLayout/assets/icon_good02.svg"
+                    alt=""
+                  />
+                  <img
+                    class="me-4"
+                    src="https://hexschool.github.io/boootstrap5WebLayout/assets/icon_good03.svg"
+                    alt=""
+                  />
+                  <img
+                    class="me-4"
+                    src="https://hexschool.github.io/boootstrap5WebLayout/assets/icon_good04.svg"
+                    alt=""
+                  />
+                </div>
+                <div class="p-3 border-left-theme bg-white mb-4">
+                  <p class="m-0">
+                    專案募資中
+                    <br />
+                    在 2021/06/14 23:59 募資結束前，您都可以贊助我們！
+                  </p>
+                </div>
+                <div class="btnGroup">
+                  <div class="row mb-2">
+                    <div class="col-md-12">
+                      <button
+                        class="btn py-2 w-100 bg-theme btnRounded border-0"
+                      >
+                        贊助專案
+                      </button>
+                    </div>
+                  </div>
+                  <div class="row g-1">
+                    <div class="col-md-8 col-6">
+                      <button
+                        class="btn btn-outline-secondary btnRounded py-2 w-100"
+                      >
+                        追蹤專案
+                      </button>
+                    </div>
+                    <div class="col-md-4 col-6">
+                      <button
+                        class="btn btn-outline-secondary btnRounded py-2 w-100"
+                      >
+                        分享
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
-<button type="button" class="btn btn-link">Link</button>
-
-
-
-<div class="card" style="width: 18rem;">
-  <img src="https://images.unsplash.com/photo-1504868584819-f8e8b4b6d7e3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZGF0YXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" class="card-img-top" alt="">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">jdiwjdiwjdowdjwoidw;oj</div>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
 
-<style lang="scss">
- 
+<style lang="scss" scoped>
+.bg-theme {
+  background-color: #ffdf65;
+}
+.subText {
+  font-family: NotoSansCJKtc-Regular;
+  font-size: 14px;
+  color: #636057;
+}
+
+.border-left-theme {
+  border-left: 3px solid #ffdf65;
+}
+
+.btnGroup {
+  color: #636057;
+  font-size: 1.25rem;
+  font-family: NotoSansCJKtc-Bold;
+  .btnRounded {
+    border-radius: 19px;
+    border: 2px solid #858377;
+  }
+}
 </style>
